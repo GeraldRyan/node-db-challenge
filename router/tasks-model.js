61a401210a8c4a014dd3e2bc66eaed147454cb24
GeraldRyan <gerald.ryan40@yahoo.com>
2020-05-15 12:40:00 -0500

    fixed bug

--- router/tasks-model.js
+++ router/tasks-model.js
@@ -2,9 +2,7 @@ const db = require('../data/dbconfig');
 
 module.exports = {
   getAllTasks,
-  addTask,
-  getTasksByProjId
-  }
+  addTask  }
 
 
   function getAllTasks() {
@@ -15,20 +13,3 @@ module.exports = {
     return db('tasks').insert({task_name: task.task_name, task_desc:task.task_desc, completed:task.completed, proj_id:task.proj_id})
   }
 
-  function getTasksByProjId(id) {
-    return db('tasks as t').leftJoin('projects as p', 't.proj_id', 'p.id').where('p.id', '=',id)
-  }
-
-  // server.get('/api/ingredients', (req, res) => {
-  //   // get all animals from the database
-  //   // include species name
-  //   db('ingredient as i')
-  //     .leftJoin('recipies as r', 'r.id', 'i.ingredient_id')
-  //     .select('i.id', 'i.ingredients_name', 'r.recipe_name')
-  //   .then(recipe => {
-  //     res.status(200).json(recipe);
-  //   })
-  //   .catch(error => {
-  //     res.status(500).json(error);
-  //   });
-  // });
